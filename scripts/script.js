b704950bf0f47b6c40f333e45121f805f68fa642
Inez-CMD <74185366+Inez-CMD@users.noreply.github.com>
2021-01-06 19:51:56 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
+
+console.log("hallo..javascript?")
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    console.log("ik werk");
+
+        if (x.style.display === "block")
+        {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block"; console.log("werk jij else?");
+        }
+    console.log("ik werk ook");
+}
+
